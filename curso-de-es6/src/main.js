ac94d227863c6cc8bc6ea90817b66d6fb905b145
JoacyMS <joacymsilva@gmail.com>
2020-03-21 23:46:46 -0300

    Início da aplicação com ES6+: estrutura e estilos

--- curso-de-es6/src/main.js
+++ curso-de-es6/src/main.js
@@ -1,16 +1,16 @@
-import "../desafios/1/exercicio1";
-import "../desafios/1/exercicio2";
-import "../desafios/1/exercicio3";
-import "../desafios/1/exercicio4";
-import "../desafios/1/exercicio5";
-import "../desafios/1/exercicio6";
-import "../desafios/1/exercicio7";
-import "../desafios/2/exercicio1";
-import "../desafios/3/exercicio1";
+// import "../desafios/1/exercicio1";
+// import "../desafios/1/exercicio2";
+// import "../desafios/1/exercicio3";
+// import "../desafios/1/exercicio4";
+// import "../desafios/1/exercicio5";
+// import "../desafios/1/exercicio6";
+// import "../desafios/1/exercicio7";
+// import "../desafios/2/exercicio1";
+// import "../desafios/3/exercicio1";
 
-const minhaPromise = () => new Promise((resolve, reject) => {
-    setTimeout(() => { resolve("OK") }, 2000)
-});
+// const minhaPromise = () => new Promise((resolve, reject) => {
+//     setTimeout(() => { resolve("OK") }, 2000)
+// });
 
 // minhaPromise()
 //     .then(response => {
@@ -20,10 +20,10 @@ const minhaPromise = () => new Promise((resolve, reject) => {
 
 //     });
 
-const executaPromise = async () => {
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-};
+// const executaPromise = async () => {
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+// };
 
-executaPromise();
+// executaPromise();
