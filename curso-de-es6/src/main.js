7ed3195853abd98a2e9ad0f94673d6d433ad5bea
JoacyMS <joacymsilva@gmail.com>
2020-03-21 23:58:35 -0300

    Aplicação com ES6+: adicionar repositórios

--- curso-de-es6/src/main.js
+++ curso-de-es6/src/main.js
@@ -27,3 +27,32 @@
 // };
 
 // executaPromise();
+
+class App {
+    constructor() {
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers () {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository (event) {
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'Rocketseat',
+            description: 'Tire a sua ideia do papel e dê vida à sua startup.',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?s=200&v=4',
+            html_url: 'https://github.com/rocketseat',
+        });
+
+        console.log(this.repositories);
+    }
+}
+
+new App();
